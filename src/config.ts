a0187522035684372c72db2c3fd0cb615ac4d652
RealRusLink <real.rus.link@gmail.com>
2026-04-15 12:59:10 +0300

    feat: config from .env

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,17 @@
-const GLOBAL_CONFIG = {
-    webPath: "/src/public",
-    listenPort: 443
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export const GLOBAL_CONFIG = {
+    webPath: process.env.WEB_PATH,
+    listenPort: parseInt(process.env.LISTEN_PORT || "80"),
+    db: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        name: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    }
 }
 
 export default GLOBAL_CONFIG
