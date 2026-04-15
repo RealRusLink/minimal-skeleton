205c8ac9f38adcccc2d365de78cf1379a7bd399d
RealRusLink <real.rus.link@gmail.com>
2026-04-15 13:08:59 +0300

    exclude compiled js

--- src/server.js
+++
@@ -1,7 +0,0 @@
-import Fastify from "fastify";
-import { registerApi } from "./routes/api.ts";
-const server = Fastify();
-registerApi(server);
-server.listen({ port: 443 }, (error, address) => {
-    console.log(error);
-});
