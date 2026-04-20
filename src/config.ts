8e2e342992c834107e3ab3a8fae19a1c105bbd89
RealRusLink <real.rus.link@gmail.com>
2026-04-20 22:17:21 +0300

    BREAKS: config refactor

--- src/config.ts
+++ src/config.ts
@@ -1,44 +1,71 @@
 import dotenv from "dotenv"
 
-dotenv.config();
 
-export const GLOBAL_CONFIG = {
-    webPath: process.env.WEB_PATH || "",
-    listenPort: parseInt(process.env.LISTEN_PORT || "80"),
+interface ConfigIF{
+    webPath: string;
+    listenPort: number;
     db: {
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT || ""),
-        name: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        usersTable: process.env.USERS_TABLE
-    }
+        host: string;
+        port: number;
+        name: string;
+        user: string;
+        password: string;
+        tables: {
+            users: string;
+        }
+    };
 }
 
-function checkConfig
-(obj:
- Record<string, any>,
- objName:
- string,
- handler:
- Function =
- (obj: Record<string, any>, objName: string, key: keyof typeof obj): void => {
-     throw new Error(`${objName}.${key} is ${obj[key]}`)
- }
-): void {
-    let key: keyof typeof obj;
-    for (key in obj) {
-        if (obj[key] === undefined || obj[key] === null || Number.isNaN(obj[key]) || obj[key] === "") {
-            handler(obj, objName, key);
-        }
-        if (typeof obj[key] === "object"){
-            console.log(`Checking ${objName}.${key}..`)
-            checkConfig(obj[key], key);
-        }
-        console.log(`${objName}.${key} is checked`)
+const ConfigBase = class {} as new () => ConfigIF
+
+export class Config extends ConfigBase{
+
+
+    constructor() {
+        dotenv.config({quiet: true})
+        super()
+        this.webPath = process.env.WEB_PATH || "";
+        this.listenPort = Number(process.env.LISTEN_PORT || 80);
+
+        this.db = {
+            host: process.env.DB_HOST || "",
+            port: Number(process.env.DB_PORT || 0),
+            name: process.env.DB_NAME || "",
+            user: process.env.DB_USER || "",
+            password: process.env.DB_PASSWORD || "",
+            tables: {
+                users: process.env.USERS_TABLE || ""
+            }
+        };
+
+        this.checkConfig(this, "GLOBAL_CONFIG");
+
     }
+
+
+    checkConfig
+    (obj:
+     Record<string, any>,
+     objName:
+     string,
+     handler:
+     Function =
+     (obj: Record<string, any>, objName: string, key: keyof typeof obj): void => {
+         throw new Error(`${objName}.${key} is ${obj[key]}. Config failed to load.`)
+     }
+    ): void {
+        let key: string;
+        for (key in obj) {
+            if (obj[key] === undefined || obj[key] === null || Number.isNaN(obj[key]) || obj[key] === "") {
+                handler(obj, objName, key);
+            }
+            if (typeof obj[key] === "object"){
+                this.checkConfig(obj[key], key);
+            }
+        }
+    }
+
 }
 
-checkConfig(GLOBAL_CONFIG, 'GLOBAL_CONFIG');
 
-export default GLOBAL_CONFIG
+export default {Config}
