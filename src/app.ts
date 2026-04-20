0a95fafe541f40e471166d471ae11a901a858913
RealRusLink <real.rus.link@gmail.com>
2026-04-20 22:19:09 +0300

    feature: proper logging & modules initialisation

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import {LoggerService, consoleConfig} from "../../logger/dist/index.js"
+import {Config} from "./config.js";
+import {DBConnector} from "./db/config.js";
+import {DBAdapter} from "./db/adapter.js";
+
+const Logger = new LoggerService(consoleConfig)
+
+
+const LoggedConfig = Logger.wrapConstructor(Config, {customMessage: "Global configuration loaded", customMessageLevel: "IMPORTANT"})
+const LoggedDBConnector = Logger.wrapConstructor(DBConnector, {customMessage: "DB pool connected", customMessageLevel: "IMPORTANT"})
+const LoggedDBAdapter = Logger.wrapConstructor(DBAdapter, {customMessage: "DB adapter initialised", customMessageLevel: "IMPORTANT"})
+
+
+const GlobalConfig = new LoggedConfig()
+const DBConnection = new LoggedDBConnector(GlobalConfig)
+const DBApi = new LoggedDBAdapter(DBConnection, GlobalConfig)
+
+
+
+
+
+
+
+
