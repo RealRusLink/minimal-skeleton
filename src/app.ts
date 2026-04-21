6738a9e68e6d90ed3cc2b3247f0d6f09b6decc13
RealRusLink <real.rus.link@gmail.com>
2026-04-21 17:14:28 +0300

    feat, breaks: new logger loading, one entry point, dep injections

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,46 @@ import {LoggerService, consoleConfig} from "../../logger/dist/index.js"
 import {Config} from "./config.js";
 import {DBConnector} from "./db/config.js";
 import {DBAdapter} from "./db/adapter.js";
+import {type RoutersDeclaration, Routes} from "./routes/index.js";
+import {Api} from "./routes/api.js";
+import {Web} from "./routes/web.js";
+import {Server} from "./server.js";
+
+
+
+
+const init = <T extends new (...args: any[]) => any>(
+    BaseClass: T,
+    message: string,
+    ...args: ConstructorParameters<T>
+): InstanceType<T> => {
+    if (!IS_LOGGING_ENABLED) return new BaseClass(...args);
+
+    const WrappedClass = Logger.wrapConstructor(BaseClass, {
+        customMessage: message,
+        customMessageLevel: "IMPORTANT"
+    });
+
+    return Logger.setMultipleLoggers(new WrappedClass(...args));
+};
+
+const IS_LOGGING_ENABLED = true;
 
 const Logger = new LoggerService(consoleConfig)
+Logger.logLevel = "INFO"
 
+const GlobalConfig = init(Config, "Global configuration loaded");
 
-const LoggedConfig = Logger.wrapConstructor(Config, {customMessage: "Global configuration loaded", customMessageLevel: "IMPORTANT"})
-const LoggedDBConnector = Logger.wrapConstructor(DBConnector, {customMessage: "DB pool connected", customMessageLevel: "IMPORTANT"})
-const LoggedDBAdapter = Logger.wrapConstructor(DBAdapter, {customMessage: "DB adapter initialised", customMessageLevel: "IMPORTANT"})
+const DBConnection = init(DBConnector, "DB pool connected", GlobalConfig);
+await DBConnection.verifyConnection();
 
+const DBApi = init(DBAdapter, "DB adapter initialised", DBConnection, GlobalConfig);
 
-const GlobalConfig = Logger.setMultipleLoggers(new LoggedConfig())
-const DBConnection = Logger.setMultipleLoggers(new LoggedDBConnector(GlobalConfig))
-await DBConnection.verifyConnection()
-const DBApi = new LoggedDBAdapter(DBConnection, GlobalConfig)
+const routersDeclaration: RoutersDeclaration = [
+    { router: init(Api, "Api routes initialised", DBApi, GlobalConfig), path: "/api" },
+    { router: init(Web, "Web routes initialised", GlobalConfig), path: "/" }
+];
 
+const AppRoutes = init(Routes, "All routes registered", routersDeclaration);
 
-
-
-
-
-
-
+const AppServer = init(Server, `Server is listening on ${GlobalConfig.listenPort}`, AppRoutes, GlobalConfig)
